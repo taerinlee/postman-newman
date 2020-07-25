647593a0326786f48d79c52e33651fb57fedeab8
Mariam Mahmoud <mariam.mahmoud.saad#gmail.com>
2020-07-25 08:58:23 +0200

    chore: run lint scripts

--- test/postmanEchoScript.js
+++ test/postmanEchoScript.js
@@ -9,17 +9,18 @@ const collection = require('../src/Postman Echo.postman_collection.json');
 
 
 const server = app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
+	console.log(`server is running on port ${PORT}`);
 });
 
 newman.run({
-    collection,
-    reporters: ['cli', 'json', 'html', 'emojitrain'],
+	collection,
+	reporters: ['cli', 'json', 'html', 'emojitrain'],
 }, async err => {
-    if(err) { throw err; }
-    console.log('collection run complete!');
+	if(err) { throw err; }
+	console.log('collection run complete!');
 
-    console.log('Server is closing..bye!');
-    server.close();
-    return process.exit(0);
+	console.log('Server is closing..bye!');
+	server.close();
+	// eslint-disable-next-line no-undef
+	return process.exit(0);
 });
